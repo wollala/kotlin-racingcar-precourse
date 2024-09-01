62638ad697e0221973f9da9aed46251300d2c426
wotjd243 <wotjd243@naver.com>
2024-09-02 06:30:03 +0900

    feat: set up the project

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "camp.nextstep.edu"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvmToolchain(17)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.10.2")
+    testImplementation("org.assertj", "assertj-core", "3.25.3")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
