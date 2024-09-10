a8c3c5213b8335399c4933cf187729297038689a
Bumjoon Park (HKMC) <bumjoon.park@hyundai.com>
2024-09-10 10:27:33 +0900

    Implement all features
    Add manual lotto

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -13,16 +13,29 @@ class LottoController(private val view: LottoView) {
         val numberOfLottos = purchaseAmount / Lotto.LOTTO_PRICE
         println("$numberOfLottos 개를 구매했습니다.")
 
+        // 1-1. 수동으로 구매할 로또 수 입력받기
+        val manualLottoCount = view.inputManualCount()
+
+        // 1-2. 수동으로 구매할 로또 번호 입력
+        val manualLottos = (1..manualLottoCount).map {
+            val numbers = view.inputManualNumbers()
+            Lotto(numbers)
+        }
+
         // 2. 로또 번호 생성
-        val purchasedLottos = generateLottos(numberOfLottos)
-        view.printLottos(purchasedLottos.map { it.numbers })
+        val purchasedLottos = generateLottos(numberOfLottos - manualLottoCount)
+        val allLottos = manualLottos + purchasedLottos
+
+        // 2-1. 수동, 자동 로또 개수 출력
+        view.printLottosType(manualLottoCount, purchasedLottos.size)
+        view.printLottos(allLottos.map { it.numbers })
 
         // 3. 당첨 번호 입력받기
         val winningNumbers = view.inputWinningNumbers()
         val bonusNumber = view.inputBonusNumber()
 
         // 4. 당첨 결과 확인
-        val lottoResults = checkLottoResults(purchasedLottos, winningNumbers, bonusNumber)
+        val lottoResults = checkLottoResults(allLottos, winningNumbers, bonusNumber)
 
         // 5. 통계 및 수익률 계산 후 출력
         val profitRate = calculateProfitRate(lottoResults, purchaseAmount)
--- src/main/kotlin/view/LottoView.kt
+++ src/main/kotlin/view/LottoView.kt
@@ -1,19 +1,33 @@
 package view
 
 class LottoView {
+    fun inputManualCount(): Int {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        return readln().toInt()
+    }
+
+    fun inputManualNumbers(): List<Int> {
+        println("수동으로 구매할 번호를 입력해 주세요.")
+        return readln().split(",").map { it.trim().toInt() }
+    }
+
     fun inputPurchaseAmount(): Int {
         println("구입금액을 입력해 주세요.")
-        return readLine()!!.toInt()
+        return readln().toInt()
     }
 
     fun inputWinningNumbers(): List<Int> {
         println("지난 주 당첨 번호를 입력해 주세요. (예: 1, 2, 3, 4, 5, 6)")
-        return readLine()!!.split(",").map { it.trim().toInt() }
+        return readln().split(",").map { it.trim().toInt() }
     }
 
     fun inputBonusNumber(): Int {
         println("보너스 볼을 입력해 주세요.")
-        return readLine()!!.toInt()
+        return readln().toInt()
+    }
+
+    fun printLottosType(manualCount: Int, autoCount: Int) {
+        println("수동으로 ${manualCount}개, 자동으로 ${autoCount}개를 구매했습니다.")
     }
 
     fun printLottos(lottos: List<List<Int>>) {
